a06fb39e64878db0f24fcd605c025017bd761450
Bruno Silva e Silva <brunosilvaesilva@hotmail.com.br>
2021-03-26 00:57:36 -0300

    Atualizado o script etl, bkp do banco de dados e consulta PBI

--- loads/transform/etl_dos_dados_via_sql.sql
+++ loads/transform/etl_dos_dados_via_sql.sql
@@ -1,19 +1,122 @@
 -- 1 Valores carregados do arquivo csv para as tabelas ETLs.
 
+--drop table ETL_CNAE
+	create table ETL_CNAE (
+					cd_cnpj				varchar(max) null ,
+					nu_ordem			varchar(max) null ,
+					cd_ramo_atividade	varchar(max) null,
+					de_ramo_atividade	varchar(max) null );
+	
+	BULK INSERT ETL_CNAE
+	FROM 'C:\CNAE.csv'
+	WITH (
+	FIELDTERMINATOR = ';',
+	rowterminator = '0x0a');
+----	
+	CREATE TABLE ETL_empresas(
+						[cd_cnpj] [varchar](max) NULL,
+						[fl_matriz] [varchar](max) NULL,
+						[dt_abertura] [varchar](max) NULL,
+						[nm_razao_social] [varchar](max) NULL,
+						[cd_natureza_juridica] [varchar](max) NULL,
+						[de_natureza_juridica] [varchar](max) NULL,
+						[nm_logradouro] [varchar](max) NULL,
+						[nu_logradouro] [varchar](max) NULL,
+						[cd_cep] [varchar](max) NULL,
+						[nm_bairro] [varchar](max) NULL,
+						[nm_municipio] [varchar](max) NULL,
+						[sg_uf] [varchar](max) NULL,
+						[de_situacao] [varchar](max) NULL,
+						[de_classif_natureza_juridica] [varchar](max) NULL);
+
+	BULK INSERT ETL_empresas
+	FROM 'C:\Users\bruno\OneDrive\Trabalho\Empresas\Neoway\Git\loads\extract\empresas.csv'
+	WITH (
+	FIELDTERMINATOR = ';',
+	rowterminator = '0x0a');
+----
+
+	CREATE TABLE ETL_empresasCalc(
+		[cd_cnpj] [varchar](max) NULL,
+		[vl_latitude] [nvarchar](max) NULL,
+		[vl_longitude] [varchar](max) NULL,
+		[vl_total_veiculos_antt] [varchar](max) NULL,
+		[vl_total_veiculos_leves] [varchar](max) NULL,
+		[vl_total_veiculos_pesados] [varchar](max) NULL,
+		[fl_optante_simples] [varchar](max) NULL,
+		[qt_filial] [varchar](max) NULL,
+		[fl_optante_simei] [varchar](max) NULL,
+		[de_saude_tributaria] [varchar](max) NULL,
+		[de_nivel_atividade] [varchar](max) NULL
+	)
+	
+	BULK INSERT ETL_empresasCalc
+		FROM 'C:\Users\bruno\OneDrive\Trabalho\Empresas\Neoway\Git\loads\extract\empresasCalc.csv'
+		WITH (
+		FIELDTERMINATOR = ';',
+		rowterminator = '0x0a');
+---
+	
+	CREATE TABLE ETL_setores_ramos_atividade(
+		[Ramo_de_Atividade] [nvarchar](50) NULL,
+		[Divisoes] [nvarchar](150) NOT NULL,
+		[CNAE_divisao_dois_digitos] [nvarchar](50) NOT NULL,
+		[F4] [nvarchar](1) NULL,
+		[F5] [nvarchar](1) NULL,
+		[F6] [nvarchar](1) NULL,
+		[F7] [nvarchar](1) NULL
+	)
+	
+	BULK INSERT ETL_setores_ramos_atividade
+		FROM 'C:\Users\bruno\OneDrive\Trabalho\Empresas\Neoway\Git\loads\extract\Setores e Ramos de Atividade.csv'
+		WITH (
+		FIELDTERMINATOR = ';',
+		rowterminator = '0x0a');
+
 --- Carrega tabela com com o cadastro do CNAE por empresas. 
 
+	--drop table d_cnae_empresas 
 	create table d_cnae_empresas (
-				[cd_cnpj]				[varchar](max) NULL,
-				[cd_cnae_dois_digitos]	[varchar](max) NULL,
-				[nu_ordem]			[varchar](max) NULL,
+				cd_cnpj					varchar(max) null, 
+				nu_ordem				varchar(max) null, 
+				cd_cnae_dois_digitos	varchar(max) null,
+				cd_ramo_atividade		varchar(max) null,
+				de_ramo_atividade		varchar(max) null
 				)
 
-	insert into d_cnae_empresas 
-	select
-		left(concat(cd_cnpj, '00'),14)				as cd_cnpj
-		,cast(left(cd_ramo_atividade, 2) as int)	as cd_cnae_dois_digitos			
-		,[nu_ordem]		
-	from ETL_CNAE
+	INSERT INTO d_cnae_empresas  (cd_cnpj, nu_ordem, cd_cnae_dois_digitos, cd_ramo_atividade, de_ramo_atividade)
+	select 
+		* 
+	from (
+		SELECT 
+			A.cd_cnpj, 
+			a.value				as nu_ordem, 
+			left(b.value, 2)	as cd_cnae_dois_digitos, 
+			b.value				as cd_ramo_atividade, 
+			c.value				as de_ramo_atividade 
+		FROM (
+			SELECT  
+				ROW_NUMBER ( ) OVER ( PARTITION BY cd_cnpj ORDER BY cd_cnpj )-1 AS RWN,
+				cd_cnpj, 
+				value
+			FROM ETL_CNAE 
+			CROSS APPLY STRING_SPLIT(replace(nu_ordem, '~@~', ';'),';')) A
+			JOIN (
+				SELECT 
+					ROW_NUMBER ( ) OVER ( PARTITION BY cd_cnpj ORDER BY cd_cnpj )-1 AS RWN, 
+					cd_cnpj, 
+					value
+				FROM ETL_CNAE  
+				CROSS APPLY STRING_SPLIT(replace(cd_ramo_atividade, '~@~', ';'),';'))B ON A.cd_cnpj = B.cd_cnpj AND A.RWN = B.RWN
+			JOIN (
+				SELECT 
+					ROW_NUMBER ( ) OVER ( PARTITION BY cd_cnpj ORDER BY cd_cnpj )-1 AS RWN ,
+					cd_cnpj, 
+					value
+				FROM ETL_CNAE 
+				CROSS APPLY STRING_SPLIT(replace(de_ramo_atividade, '~@~', ';'),';'))C ON A.cd_cnpj = C.cd_cnpj AND A.RWN = C.RWN
+			) d
+	order by cd_cnpj 
 
 --- Carrega tabela com o cadastro das empresas. 
 
@@ -93,11 +196,11 @@
 	SELECT 
 		  [Ramo_de_Atividade]
 	      ,[Divisoes]
-	      ,[cd_CNAE_dois_digitos]
+	      ,[CNAE_divisao_dois_digitos] as [cd_CNAE_dois_digitos]
 	  FROM [dbo].[ETL_setores_ramos_atividade]
 
 
-  ---Relacionamento entre dimensões
+  ---Relacionamento entre dimensões (Utilizado no primeiro modelo)
 
 	select 
 		e.cd_cnpj, c.cd_cnae_dois_digitos
